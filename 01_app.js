7e4474a8ac87cb8a26ccde6a5a7d895ef05f7a70
emileprogrameur <emile_bisaillon@hotmail.fr>
2018-02-09 13:27:49 -0500

    arboressance de base

---
+++ 01_app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+   fs.readFile( __dirname + "/public/data/" + "membres.txt", 
+        'utf8',
+        (err, data) => {if (err) { return console.error(err);}
+        	console.log( data );
+        	let resultat = JSON.parse(data);           
+  		res.render('gabarit.ejs', {adresses: resultat})  
+  	});
+})
+
+const server = app.listen(8080, function () {
+   const host = server.address().address
+   const port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
