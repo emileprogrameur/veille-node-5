d65edc1b3747347bde66c6abfdcf7141677c9de8
emileprogrameur <emile_bisaillon@hotmail.fr>
2018-02-14 09:14:01 -0500

    connexion mongoDB et route

--- 01_app.js
+++ 01_app.js
@@ -1,36 +1,29 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
+const express = require('express')
+const app = express()
+const fs = require('fs')
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-
-let db // variable qui contiendra le lien sur la BD
-
-MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
- if (err) return console.log(err)
- db = database
-// lancement du serveur Express sur le port 8081
- app.listen(8081, () => {
- console.log('connexion à la BD et on écoute sur le port 8081')
- })
-})
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function (req, res) {
-    let cursor = db.collection('adresse')
-                .find().toArray(function(err, resultat){
- 			if (err) return console.log(err)
- 		// transfert du contenu vers la vue index.ejs (renders)
- 		// affiche le contenu de la BD          
-  		res.render('gabarit.ejs', {adresses: resultat})  
-  	});
+  let cursor = db.collection('adresse').find().toArray(function(err, resultat){
+ 	if (err) return console.log(err)
+ 	console.log(JSON.stringify(resultat))
+ 	// transfert du contenu vers la vue index.ejs (renders)
+ 	// affiche le contenu de la BD          
+  res.render('gabarit.ejs', {adresses: resultat})  
+  })
 })
 
-const server = app.listen(8080, function () {
-   const host = server.address().address
-   const port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+/*----------------------Connexion à MongoDB et au serveur Node.js-----------------------*/
+let db // variable qui contiendra le lien sur la BD
+MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
+	if (err) return console.log(err)
+	db = database.db('carnet_adresse2')
+	// lancement du serveur Express sur le port 8081
+	app.listen(8081, () => {
+	 	console.log('connexion à la BD et on écoute sur le port 8081')
+	})
 })
